1009fc7b7a88f359a861b08fbef711ecd59c5da5
ali <abaykal@icrc.org>
2016-08-17 19:40:35 +0200

    added bundled version for serverles testing

---
+++ webpack.conf.js
@@ -0,0 +1,14 @@
+var path = require("path");
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.json$/, loader: "json"}
+        ]
+    }
+};
