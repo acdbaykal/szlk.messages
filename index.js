bb4de997bae7c2ae8fa1320760a1b2e746ccb3ce
ali <abaykal@icrc.org>
2016-08-17 14:54:09 +0200

    added an object about supported languages, expanded the test, translated language names

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+var supported = require("./supported_languages.json");
+
 function getMessages(lang){
   return require("./messages/"+lang+".json");
 }
+
+getMessages.supportedLanguages = supported;
 
 module.exports = getMessages;
--- test.js
+++ test.js
@@ -1,12 +1,25 @@
-var test_json = require("./index")("test");
+var _ = require("./index");
+var messages = _("test");
 
-var type = typeof test_json;
-if( type !== "object"){
-  console.error("Instead of an object " + type + " was returned");
+if(typeof _ !== "function"){
+  console.error("Instead of an object " + typeof _ + " was returned");
   process.exit(1);
 }
 
-var val = test_json["key"];
+if( typeof _.supportedLanguages !== "object"){
+  console.error("Imported object does not contain information about supported languages.");
+  process.exit(1);
+}
+
+var messages_type = typeof messages;
+if( messages_type !== "object"){
+  console.error("Instead of an object " + messages_type + " was returned");
+  process.exit(1);
+}
+
+
+
+var val = messages["key"];
 if( val !== "value"){
   console.error("key property returned " + val + " instead of 'value'");
   process.exit(1);
